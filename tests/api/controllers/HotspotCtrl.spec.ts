d3643c6389886c3ff6d4f101112be7f29f7f035b
Lionnel DUPOUY <l.dupouy@lectra.com>
2017-11-27 13:55:47 +0100

    chore: refactor jwt parser mock

--- tests/api/controllers/HotspotCtrl.spec.ts
+++ tests/api/controllers/HotspotCtrl.spec.ts
@@ -1,4 +1,6 @@
 // tslint:disable-next-line:import-name
+import { createHotspot } from '../../../src/infrastructure/HotspotFactory';
+import CityzenSample from '../../../src/domain/cityzens/model/CityzenSample';
 import { Response } from '_debugger';
 import JwtParser from '../../../src/api/services/auth/JwtParser';
 import * as querystring from 'querystring';
@@ -17,26 +19,6 @@ describe('HotspotCtrl', () => {
     let nextMoq : TypeMoq.IMock<rest.Next>;
     let hotspotRepositoryMoq : TypeMoq.IMock<HotspotRepositoryInMemory>;
     let jwtParser : TypeMoq.IMock<JwtParser>;
-    let north : number;
-    let south : number;
-    let west : number;
-    let east : number;
-    let queryStrings : any;
-    let repositoryResult : any;
-
-    before(() => {
-        north = 1.111;
-        south = -2.434;
-        west = 9.23322;
-        east = -2.1111;
-        repositoryResult = [HotspotSample.CHURCH, HotspotSample.SCHOOL];
-        queryStrings = {
-            north,
-            south,
-            east,
-            west,
-        };
-    });
 
     beforeEach(() => {
         reqMoq = TypeMoq.Mock.ofType<rest.Request>();
@@ -47,6 +29,28 @@ describe('HotspotCtrl', () => {
     });
 
     describe('hotspots', () => {
+
+        let north : number;
+        let south : number;
+        let west : number;
+        let east : number;
+        let queryStrings : any;
+        let repositoryResult : any;
+
+        before(() => {
+            north = 1.111;
+            south = -2.434;
+            west = 9.23322;
+            east = -2.1111;
+            repositoryResult = [HotspotSample.CHURCH, HotspotSample.SCHOOL];
+            queryStrings = {
+                north,
+                south,
+                east,
+                west,
+            };
+        });
+
         it(
         `should respond with 200 and a list of hotspot
         if queryStrings contains valid coords`,
@@ -96,6 +100,45 @@ describe('HotspotCtrl', () => {
             .verify(
                 x => x.json(200, []),
                 TypeMoq.Times.once());
+        });
+    });
+
+    describe('postHotspots', () => {
+
+        let jsonBody : any;
+
+        before(() => {
+            jsonBody = {
+                title: 'my new hotspot',
+                position: {
+                    latitude: 12.23323,
+                    longitude: 22.1112221,
+                },
+                id_city: '33273',
+                message: 'a classic message',
+                newAttr: 'random value',
+                scope: 'private',
+            };
+        });
+
+        it.skip ('should create a new hotspot and return it with 200 OK', () => {
+            // Arrange
+            reqMoq
+            .setup((x : rest.Request) => x.body)
+            .returns(() => jsonBody);
+
+            const fakeCityzenToSave = createHotspot(jsonBody);
+
+            // Act
+            new HotspotCtrl(jwtParser.object, hotspotRepositoryMoq.object).postHotspots(
+                reqMoq.object,
+                resMoq.object,
+                nextMoq.object,
+            );
+
+            // Assert
+            hotspotRepositoryMoq.verify(x => x.store(fakeCityzenToSave), TypeMoq.Times.once());
+            resMoq.verify(x => x.json(201, fakeCityzenToSave), TypeMoq.Times.once());
         });
     });
 });
--- tests/api/controllers/ProfileCtrl.spec.ts
+++ tests/api/controllers/ProfileCtrl.spec.ts
@@ -14,6 +14,7 @@ import * as rest from 'restify';
 import cityzenAuth0Repository, { CityzenAuth0Repository } from './../../../src/infrastructure/CityzenAuth0Repository';
 import { Auth0 } from '../../../src/api/libs/Auth0';
 import { OK } from 'http-status-codes';
+import * as sample from './sample';
 
 describe('ProfileCtrl', () => {
 
@@ -22,35 +23,21 @@ describe('ProfileCtrl', () => {
     let nextMoq : TypeMoq.IMock<rest.Next>;
     let cityzenRepositoryMoq : TypeMoq.IMock<CityzenAuth0Repository>;
     let auth0SdkMoq : TypeMoq.IMock<Auth0>;
-    let fakeDecodedToken : any;
-    let jwtParser : TypeMoq.IMock<JwtParser>;
+    let jwtParserMoq : TypeMoq.IMock<JwtParser>;
     let profileCtrl : ProfileCtrl;
     let hotspotRepositoryMoq : TypeMoq.IMock<HotspotRepositoryInMemory>;
 
     // simule la vérification et decode le token d'authentification
     before(async () => {
-        // represente un jwt token décodé
-        fakeDecodedToken = {
-            sub: 'fake-id',
-            email: 'fake@email.com',
-            nickname: 'Alice',
-            [`${config.auth.auth0JwtPayloadNamespace}/user_metadata`]: {
-                favoritesHotspots: ['an-hotspot-id'],
-            },
-        };
-        resMoq = TypeMoq.Mock.ofType<rest.Response>();
-        nextMoq = TypeMoq.Mock.ofType<rest.Next>();
 
         // mock la lecture du header http contenant le jwt
-        reqMoq = TypeMoq.Mock.ofType<rest.Request>();
-        const authorizationHeader = 'Bearer fake.jwt.token';
-        reqMoq.setup(x => x.header('Authorization')).returns(() => authorizationHeader);
-
         // simule la validation du jwt token
-        jwtParser = TypeMoq.Mock.ofType<JwtParser>();
-        jwtParser
-        .setup(x => x.verify(authorizationHeader.slice(7)))
-        .returns(() => Promise.resolve(fakeDecodedToken));
+        reqMoq = TypeMoq.Mock.ofType<rest.Request>();
+        jwtParserMoq = TypeMoq.Mock.ofType<JwtParser>();
+        sample.setupReqAuthorizationHeader(reqMoq, jwtParserMoq);
+
+        resMoq = TypeMoq.Mock.ofType<rest.Response>();
+        nextMoq = TypeMoq.Mock.ofType<rest.Next>();
 
         auth0SdkMoq = TypeMoq.Mock.ofType<Auth0>();
         cityzenRepositoryMoq = TypeMoq.Mock.ofType<CityzenAuth0Repository>();
@@ -58,7 +45,7 @@ describe('ProfileCtrl', () => {
 
         // instanciation du ProfilCtrl
         profileCtrl = new ProfileCtrl(
-            jwtParser.object,
+            jwtParserMoq.object,
             cityzenRepositoryMoq.object, auth0SdkMoq.object, hotspotRepositoryMoq.object);
 
         // appel du middleware de control d'acces de l'utilsateur
@@ -111,12 +98,11 @@ describe('ProfileCtrl', () => {
             .setup(x => x.findById(params.favoritHotspotId))
             .returns(() => HotspotSample.CHURCH);
 
-            const userMetadataKey = `${config.auth.auth0JwtPayloadNamespace}/user_metadata`;
             const cityzen = new Cityzen(
-                fakeDecodedToken.sub,
-                fakeDecodedToken.email,
-                fakeDecodedToken.nickname,
-                fakeDecodedToken[userMetadataKey].favoritesHotspots,
+                profileCtrl.decodedJwtPayload.sub,
+                profileCtrl.decodedJwtPayload.email,
+                profileCtrl.decodedJwtPayload.nickname,
+                profileCtrl.decodedJwtPayload.userMetadata.favoritesHotspots,
             );
             cityzen.addHotspotAsFavorit(params.favoritHotspotId);
 
