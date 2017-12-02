59c83aa24f042191206df09106daa78dcf3cbced
Luca Bourroux <lucabrx@gmail.com>
2017-12-02 01:28:47 +0100

    little fix, Merignac was in the saraha
    (hey it's my first commit ! :D)

--- src/domain/cityLife/model/sample/PositionSample.ts
+++ src/domain/cityLife/model/sample/PositionSample.ts
@@ -6,7 +6,7 @@ class PositionSample {
     public static TOWNHALL = new Position(44.84032108, -0.77510476);
     public static CHURCH = new Position(44.84020316, -0.77552855);
     public static SCHOOL = new Position(44.84665782, -0.76560438);
-    public static MERIGNAC = new Position(22.84665782, -0.16560438);
+    public static MERIGNAC = new Position(44.84181, -0.64759);
 }
 
 export default PositionSample;
