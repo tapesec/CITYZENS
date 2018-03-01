c5df6736f5182d304256a9343c7a1e002dde2d3f
Tackwin <tackwinbrx@hotmail.fr>
2018-03-01 02:21:41 +0100

    Added TUs. Added options to specify delay between request. Implemented suggestions by Tapesec.

--- src/api/services/errors/retryPromise.ts
+++ src/api/services/errors/retryPromise.ts
@@ -1,20 +1,50 @@
 
-export default <T>(call: any, opts: any): Promise<T> => {
-    const retries = opts.retries;
+const defaultOpts = {
+    retries: 5,
+    delayBase: 2,
+    delayMultiple: 100,
+    delayCustom: undefined as number[],
+};
 
-    return new Promise<T>(async (resolve, reject) => {
-        let err;
-        for (let i = 0; i < retries; i += 1) {
-            try {
-                await call().then((r: T) => {
-                    resolve(r);
-                });
-                return;
-            } catch (error) {
-                err = error;
+const sleep = (ms: number): Promise<{}> => {
+    return new Promise(r => setTimeout(r, ms));
+};
+
+export default async <T>(call: () => Promise<T>, _opts?: any): Promise<T> => {
+    const opts = _opts || defaultOpts;
+
+    const retries = opts.retries ? opts.retries : defaultOpts.retries;
+    const delayBase = opts.delayBase ? opts.delayBase : defaultOpts.delayBase;
+    const delayMultiple = opts.delayMultiple
+        ? opts.delayMultiple
+        : defaultOpts.delayMultiple; // in ms.
+
+    // just a quick check to make sure the array passed is of good dimensions.
+    const delayCustom =
+        (opts.delayCustom instanceof Array &&
+         opts.delayCustom.length() === retries)
+            ? opts.delayCustom
+            : defaultOpts.delayCustom;
+
+    let result: T;
+    let err: any;
+
+    let lastDelay = delayMultiple;
+
+    for (let i = 0; i < retries; i += 1) {
+        try {
+            result = (await call());
+            return result;
+        } catch (error) {
+            err = error;
+            await sleep(lastDelay);
+
+            lastDelay *= delayBase;
+            if (delayCustom) {
+                lastDelay = delayCustom[i];
             }
         }
-        reject(err);
-    });
+    }
 
+    throw err;
 };
