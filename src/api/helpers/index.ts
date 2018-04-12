faf0e2b74ba1d493d3f4c228ecb044a8854af822
Tackwin <tackwinbrx@hotmail.fr>
2018-04-12 21:53:32 +0200

    Fix: bug in validation to GET /hotspots. (jsonschema validate NaN as number). Added two TU and one TI to test for this bug.

--- src/api/helpers/index.ts
+++ src/api/helpers/index.ts
@@ -1,7 +1,9 @@
+import { isNumber } from 'util';
+
 export const strToNumQSProps = (queryStrings: any, props: string[]) => {
     props.forEach(prop => {
         if (queryStrings[prop]) {
-            queryStrings[prop] = +queryStrings[prop];
+            if (!isNaN(+queryStrings[prop])) queryStrings[prop] = +queryStrings[prop];
         }
     });
     return queryStrings;
